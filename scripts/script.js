f5931181b06e841ad99fbba753fbda5279f9d8e3
gijscreator <65440619+gijscreator@users.noreply.github.com>
2026-01-13 11:57:56 +0100

    production version for nav buttons per day

--- scripts/script.js
+++ scripts/script.js
@@ -194,3 +194,33 @@ if (playButton) {
     }
   });
 }
+
+const slider = document.querySelector('.sync-scroll');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active'); // for cursor change
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // scroll-fast multiplier
+  slider.scrollLeft = scrollLeft - walk;
+});
