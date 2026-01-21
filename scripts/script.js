45c81e2e42e1b2f406eb3aec86d04c802382c014
gijscreator <65440619+gijscreator@users.noreply.github.com>
2026-01-21 11:57:10 +0100

    combined styleguide with style.css for better performance

--- scripts/script.js
+++ scripts/script.js
@@ -127,12 +127,12 @@ const UserInterface = {
                         <h3 class="fly-in-text title">${program.show_name}</h3>
                         <p class="time">${program.from} - ${program.until}</p>
                         <div class="wrapper">
-                        ${isLiveNow ? '<p class="live">Live</p>' : ''}
-                        <p class="link">
-                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M7.5 15L12.5 10L7.5 5" stroke="white" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-                        </p>
+                            ${isLiveNow ? '<p class="live">Live</p>' : ''}
+                            <p class="link">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M7.5 15L12.5 10L7.5 5" stroke="white" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </p>
                         </div>
                     </section>
                 </article>
